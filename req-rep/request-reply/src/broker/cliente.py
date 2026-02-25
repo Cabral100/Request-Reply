7a65837bed89e87d36f8938f7006f4d1869cb4aa
Lucas Tonoli Cabral Duarte <146139708+Cabral100@users.noreply.github.com>
2026-02-25 21:50:16 +0000

    finalizando request-reply

--- req-rep/request-reply/src/broker/cliente.py
+++ req-rep/request-reply/src/broker/cliente.py
@@ -1,4 +1,5 @@
 import zmq
+import json
 from time import sleep
 
 context = zmq.Context()
@@ -7,9 +8,38 @@ socket.connect("tcp://broker:5555")
 
 i = 0
 while True:
-    print(f"Mensagem {i}:", end=" ", flush=True)
-    socket.send(b"Hello")
-    mensagem = socket.recv()
-    print(f"{mensagem}")
-    i += 1
-    sleep(0.5)
+
+    print("==== MENU ====")
+    print("1 - Criar")
+    print("2 - Deletar")
+    print("3 - Listar")
+    
+    
+    opcao = input("Escolha uma opção:")
+
+    if opcao == "1":
+        msg = {"funcao": "criar", "dados": "teste"}
+        msg = json.dumps(msg)
+        print(f"{msg}", flush = True)
+        socket.send_string(msg)
+        mensagem = socket.recv()
+        print(f"{mensagem}", flush=True)
+        sleep(0.5)
+
+    elif opcao == "2":
+        msg = {"funcao": "deletar", "dados": "teste"}
+        msg = json.dumps(msg)
+        print(f"{msg}", flush = True)
+        socket.send_string(msg)
+        mensagem = socket.recv()
+        print(f"{mensagem}", flush=True)
+        sleep(0.5)
+
+    elif opcao == "3":
+        msg = {"funcao": "listar", "dados": "teste"}
+        msg = json.dumps(msg)    
+        print(f"{msg}", flush = True)
+        socket.send_string(msg)
+        mensagem = socket.recv()
+        print(f"{mensagem}", flush=True)
+        sleep(0.5)
--- req-rep/request-reply/src/broker/servidor.py
+++ req-rep/request-reply/src/broker/servidor.py
@@ -1,11 +1,34 @@
 import zmq
+import json 
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.connect("tcp://broker:5556")
 
-while True:
-    message = socket.recv()
-    print(f"Mensagem recebida: {message}", flush=True)
-    socket.send_string("World")
+tarefas = list()
 
+while True:
+    msg = socket.recv_string()
+    msg = json.loads(msg)
+
+    funcao = msg.get("funcao", "")
+    dados = msg.get("dados", "")
+
+    if funcao == "criar":
+        tarefas.append(dados)
+        socket.send_string("ok")
+
+    elif funcao == "deletar":
+        if dados in tarefas:
+            tarefas.remove(dados)
+            socket.send_string("ok")
+        else:
+            socket.send_string("erro")
+
+    elif funcao == "listar":
+        socket.send_string(json.dumps(tarefas))
+
+    else:
+        socket.send_string("erro")
+
+    print(tarefas, flush=True)
